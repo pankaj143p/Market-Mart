4a5f9fb63a701fac6868b2f83a5b3be339a9f117
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-04-11 02:26:32 +0530

    add admin pannel

---
+++ AdminPannel/src/components/dashboard/layout/config.ts
@@ -0,0 +1,12 @@
+import type { NavItemConfig } from '@/types/nav';
+import { paths } from '@/paths';
+
+export const navItems = [
+  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'products', title: 'Products', href: paths.dashboard.Products, icon: 'ph-bag' },
+  { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
+  { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
+  { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
+  { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
+] satisfies NavItemConfig[];
---
+++ AdminPannel/src/paths.ts
@@ -0,0 +1,13 @@
+export const paths = {
+  home: '/',
+  auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
+  dashboard: {
+    Products: '/dashboard/products',
+    overview: '/dashboard',
+    account: '/dashboard/account',
+    customers: '/dashboard/customers',
+    integrations: '/dashboard/integrations',
+    settings: '/dashboard/settings',
+  },
+  errors: { notFound: '/errors/not-found' },
+} as const;
