37d977b3bcb71667c5bd460670ecb1b0f9ac23fb
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-07-21 18:15:32 +0530

    add admin route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 const userSignUpController = require("../controller/user/userSignUp")
+const adminSignUpController = require("../controller/user/adminSignUp")
 const userSignInController = require('../controller/user/userSignIn')
 const userDetailsController = require('../controller/user/userDetails')
 const authToken = require('../middleware/authToken')
@@ -26,6 +27,7 @@ const filterProductController = require('../controller/product/filterProduct')
 
 // user pannel 
 router.post("/signup",userSignUpController)
+router.post("/adminsignup",adminSignUpController)
 router.post("/signin",userSignInController)
 router.get("/user-details",authToken,userDetailsController)
 router.get("/userLogout",userLogout)
