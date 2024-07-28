265d83b918d10a6bf65cee10e14526765b037f52
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-07-28 20:40:51 +0530

    temp sign remove

--- my-shop-master/pages/api/auth/[...nextauth].js
+++ my-shop-master/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth({
+export default NextAuth({ 
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
