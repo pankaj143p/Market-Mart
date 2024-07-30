75741eb2397233eca3d3d365a3e79eff582a625d
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-07-30 20:38:13 +0530

    add all

--- my-shop-master/models/Category.js
+++
@@ -1,8 +0,0 @@
-const { Schema, models, model, default: mongoose } = require("mongoose");
-
-const CategorySchema = new Schema({
-  name: {type: String, required: true},
-  parent: {type:mongoose.Types.ObjectId, ref:'Category'},
-})
-
-export const Category = models?.Category || model('Category', CategorySchema);
--- my-shop-master/my-shop-master/pages/components/Spinner.js
+++
@@ -1,7 +0,0 @@
-import { FadeLoader } from "react-spinners";
-
-export default function Spinner() {
-  return <>
-    <FadeLoader color="#d66b36" />
-  </>
-}
