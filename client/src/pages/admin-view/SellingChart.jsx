37f815f2b13ada0d40619bb4eca8fe74d6b52db9
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-09-12 23:47:32 +0530

    chart section

--- client/src/pages/admin-view/SellingChart.jsx
+++ client/src/pages/admin-view/SellingChart.jsx
@@ -1,9 +1,12 @@
 
 import { ResponsivePie } from '@nivo/pie'
+import AdminSellingChartView from '@/components/admin-view/sellingchart'
 // import data from './tempData'
 const SellingChart= ({ data /* see data tab */ }) => (
    <>
-     This is selling chart
+    <div>
+      <AdminSellingChartView />
+    </div>
    </>
 )
 export default SellingChart;
