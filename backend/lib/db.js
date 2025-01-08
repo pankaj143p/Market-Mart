813fb33758d0d13b66635bef1d11c06ad58e2e39
pankaj143p <pankaj114477pankaj@gmail.com>
2025-01-08 12:44:00 +0530

    upadate en

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -37,7 +37,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/newecom");
+        const conn = await mongoose.connect( process.env.MONGO_URL || "mongodb://localhost:27017/marketmart");
         console.log(`MongoDB connected: ${conn.connection.host,conn}`);
     } catch (error) {
         console.log("Error connecting to MONGODB", error.message);
